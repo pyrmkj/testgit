8ae735ead8c59b8ae4206cfe19a66f58ea7ad5b4
zhubh.zyyt <305845802@qq.com>
2018-01-06 23:26:44 +0800

    修改方法了

--- src/main/java/com/xxzx/TestgitApplication.java
+++ src/main/java/com/xxzx/TestgitApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestgitApplication {
-
+    public String test(){
+		System.out.println("1212");
+		return "test";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TestgitApplication.class, args);
 	}
